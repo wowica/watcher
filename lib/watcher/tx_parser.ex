7958c52b155d3823d023e490d9233f6979460c60
Carlos Souza <carloshrsouza@gmail.com>
2023-07-03 19:22:59 -0400

    Add parser for OuraV2 Redis payload format

--- lib/watcher/tx_parser.ex
+++ lib/watcher/tx_parser.ex
@@ -21,7 +21,30 @@ defmodule Watcher.TxParser do
     Logger.info("Address #{address} received #{div(amount, 1_000_000)} ADA at #{timestamp}")
   end
 
-  def parse(payload) do
-    Logger.info("Got message from stream #{inspect(payload)}")
+  # This function is for OuraV2
+  # Strictly used for local development for the time being.
+  def parse(event_payload) do
+    # The new redis entry format for OuraV2 is a single key
+    # being the timestamp of the entry.
+    [event] = Map.values(event_payload)
+
+    case Jason.decode!(event) do
+      %{
+        "context" => %{"output_address" => addr},
+        "output_asset" => output_asset
+      } = _event_data_decoded
+      when not is_nil(addr) ->
+        %{"amount" => amount, "asset_ascii" => asset_name} = output_asset
+        Logger.info("address #{addr}\t amount: #{amount} #{asset_name}")
+        :ok
+
+      %{"tx_output" => %{"address" => addr, "amount" => amount}} ->
+        Logger.info("address #{addr}\t amount: #{amount} ADA")
+        :ok
+
+      decoded ->
+        Logger.info("No output address #{inspect(Map.keys(decoded))}")
+        :ok
+    end
   end
 end
