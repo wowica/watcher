191ecd7a5551107864583a16909ab55d9d76ca02
Carlos Souza <carloshrsouza@gmail.com>
2023-09-29 10:00:38 -0400

    Test TxBuffer returns txs

--- test/tx_buffer_test.exs
+++ test/tx_buffer_test.exs
@@ -25,12 +25,13 @@ defmodule Watcher.TxBufferTest do
   end
 
   test "flushes when buffer is met", %{test: name} do
-    pid = start_supervised!({TxBuffer, name: name, buffer_size: 2})
+    pid = start_supervised!({TxBuffer, name: name, buffer_size: 3})
 
     my_pid = self()
 
     tx1 = "foo"
     tx2 = "bar"
+    tx3 = "baz"
 
     _ =
       TxBuffer.add_tx(pid, tx1, fn _txs ->
@@ -38,14 +39,24 @@ defmodule Watcher.TxBufferTest do
       end)
 
     refute_receive :did_not_flush_to_socket, 500
+    assert [^tx1] = TxBuffer.get_txs(pid)
 
     _ =
       TxBuffer.add_tx(pid, tx2, fn _txs ->
+        send(my_pid, :did_not_flush_to_socket)
+      end)
+
+    refute_receive :did_not_flush_to_socket, 500
+
+    all_txs = TxBuffer.get_txs(pid)
+    assert Enum.all?([tx1, tx2], &(&1 in all_txs))
+
+    _ =
+      TxBuffer.add_tx(pid, tx3, fn _txs ->
         send(my_pid, :flush_to_socket)
       end)
 
     assert_receive :flush_to_socket, 500
-
     assert [] == TxBuffer.get_txs(pid)
   end
 end
